1a6fe592cce1a06f4c7d9372a1fbf2b2a799ba7f
farid <deltabur@gmail.com>
2025-03-26 12:27:59 -0400

    Starting the project

---
+++ backend/src/main/java/com/faridkaki/Aerobot/AerobotApplication.java
@@ -0,0 +1,13 @@
+package com.faridkaki.Aerobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerobotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/faridkaki/Aerobot/AerobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faridkaki.Aerobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
